6c37f26b2ab7c6e5eee5cf226b8fafedbd4455ce
KateKrav <laylovalova@gmail.com>
2020-10-05 15:48:41 +0300

    - add let , add function;-JS; _add css

--- code.js
+++ code.js
@@ -6,6 +6,11 @@ let refrigerator = ["макароны", "салат", "фрукты", "сок","
 let gasStove = ["жаренная курочка", "суп", "борщ", "пюре"];
 let cupboard= ["сухарики", "чипсы", "печенье", "шоколад"];
 let i;
+let str = ' ';
+let str1 = ' ';
+let str2 = ' ';
+
+
 
 function getFood() {
     getStatus = document.getElementById("inputMessage").value;
@@ -19,10 +24,26 @@ function getFood() {
         default:
             console.log("There can be no other options");
     }
+
 }
 
 function findFoodInRefrigerator () {
-    for (i = 0, i < refrigerator.length; i++){
-        document.getElementById("show_list").innerHTML= refrigerator.length[i]
+    for (i = 0; i < refrigerator.length; i++)
+        if (refrigerator[i]!==undefined) str += refrigerator[i]+'<br>';{
+        document.getElementById("show_listRefrigerator").innerHTML = str;
+    }
+}
+
+function findFoodInGasStove () {
+    for (i = 0; i < gasStove.length; i++)
+        if (gasStove[i]!==undefined) str1 += gasStove[i]+'<br>';{
+        document.getElementById("show_listGasStove").innerHTML = str1;
+    }
+}
+
+function findFoodInCupboard () {
+    for (i = 0; i < cupboard.length; i++)
+        if (cupboard[i]!==undefined) str2 += cupboard[i]+'<br>';{
+        document.getElementById("show_listCupboard").innerHTML = str2;
     }
 }
