c8759b02e131e0f72d07c852075623ac6b189b8d
KateKrav <laylovalova@gmail.com>
2020-10-05 14:14:22 +0300

    - code amendments

--- code.js
+++ code.js
@@ -1,26 +1,11 @@
 "use strict";
 
-let a;
-let i;
 
-function amount_in_square() {
-    a = document.getElementById("integerNumber").value;
-
-    for (i = 0; i < a; i++) {
-        if(i % 2 ===0){
-            console.log(i);
-        }else{
-        }
-    }
-}
-
-
-let  status;
 let getStatus;
 let refrigerator = ["макароны", "салат", "фрукты", "сок","бутерброт"];
 let gasStove = ["жаренная курочка", "суп", "борщ", "пюре"];
 let cupboard= ["сухарики", "чипсы", "печенье", "шоколад"];
-
+let i;
 
 function getFood() {
     getStatus = document.getElementById("inputMessage").value;
