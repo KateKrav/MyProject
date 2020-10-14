cf3c4bb25e739689a862e20f1bd44d8287c76bf8
KateKrav <laylovalova@gmail.com>
2020-10-15 00:07:53 +0300

    -new branch

---
+++ code.js
@@ -0,0 +1,10 @@
+let generalList =  [ "вася", "петя", "маша", "даша", "женя"];
+let userName;
+let findName = "";
+
+function findNameInList(){
+    userName = document.getElementById("name_user").value ;
+  for (let i = 0; i < generalList.length; i++){
+      console.log(generalList[i]);
+  }
+}
