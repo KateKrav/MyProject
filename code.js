d13ec43427b38d671891027db346a8a0bd638cb2
KateKrav <laylovalova@gmail.com>
2020-10-15 14:37:43 +0300

    - function "find name"

--- code.js
+++ code.js
@@ -5,6 +5,12 @@ let findName = "";
 function findNameInList(){
     userName = document.getElementById("name_user").value ;
   for (let i = 0; i < generalList.length; i++){
-      console.log(generalList[i]);
+      // console.log(generalList[i]);
+      if (userName === generalList[i]){
+          console.log(userName + " проходи!");
+      }else {
+          console.log("Проваливай!")
+      }
+
   }
 }
