b8559319c9641783452d065cbf00eea8901e20b7
KateKrav <laylovalova@gmail.com>
2020-10-15 15:43:57 +0300

    - fixed function

--- code.js
+++ code.js
@@ -1,18 +1,25 @@
 let generalList =  [ "вася", "петя", "маша", "даша", "женя"];
 let userName;
 let findName = "";
+let nameOfList;
+let messageUser;
+let warning;
 
 function findNameInList(){
     userName = document.getElementById("name_user").value ;
   for (let i = 0; i < generalList.length; i++){
       findName = "";
       findName = findName + generalList[i];
-      // console.log(generalList[i]);
-      if (userName === findName){
-          console.log(userName + " проходи!");
-      }else {
-          console.log("Проваливай!")
+
+      if (userName === findName) {
+          messageUser = userName + " проходи!";
+          document.getElementById("showMessage").innerHTML = messageUser;
+
       }
   }
 
+  if (userName!== findName){
+      warning = "Проваливай";
+      document.getElementById("showMessage").innerHTML = warning;
+  }
 }
