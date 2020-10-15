e66ff70923dac9cb2e71d5e87ae47ab7151aa36c
Alexey Usatenko <qallekso@gmail.com>
2020-10-15 16:00:57 +0300

    refactor code

--- code.js
+++ code.js
@@ -2,24 +2,21 @@ let generalList =  [ "вася", "петя", "маша", "даша", "женя"]
 let userName;
 let findName = "";
 let nameOfList;
-let messageUser;
+let decision;
 let warning;
 
 function findNameInList(){
     userName = document.getElementById("name_user").value ;
   for (let i = 0; i < generalList.length; i++){
-      findName = "";
-      findName = findName + generalList[i];
 
-      if (userName === findName) {
-          messageUser = userName + " проходи!";
-          document.getElementById("showMessage").innerHTML = messageUser;
-
+      if (userName === generalList[i]) {
+          decision = ", проходи!";
+          console.log(decision);
       }
+      else decision = ", проваливай!"
+      console.log(decision);
   }
 
-  if (userName!== findName){
-      warning = "Проваливай";
-      document.getElementById("showMessage").innerHTML = warning;
-  }
+    document.getElementById("showMessage").innerHTML = userName + decision;
+
 }
