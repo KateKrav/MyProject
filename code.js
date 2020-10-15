b9b485bda1423f46745dcc98a4fa5ef5802b8239
KateKrav <laylovalova@gmail.com>
2020-10-15 22:06:56 +0300

    - add array black list

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
 let generalList = ["вася", "петя", "маша", "даша", "женя"];
+let blackList = ["фома", "гия", "жора"];
 let userName;
 let decision = false;
+let message;
 
 function findNameInList() {
     userName = document.getElementById("name_user").value;
@@ -9,11 +11,24 @@ function findNameInList() {
         if (userName === generalList[i]) {
             decision = true;
             // console.log(decision);
-        }
+        } // let message = ", УХОДИ";
+        findNameInBlackList();
     }
-    let message = ", УХОДИ";
+
     if (decision === true) {
         message = ", ПРОХОДИ"
     }
     document.getElementById("showMessage").innerHTML = userName + message;
+}
+
+
+function findNameInBlackList() {
+    for(let i = 0; i< blackList.length; i++){
+        if (userName === blackList[i]){
+            decision = true;
+            message = ", простите, мест нет"
+        }
+
+    }
+
 }
