efa9147c3c1de965c7a715993f682866bde09f08
KateKrav <laylovalova@gmail.com>
2020-10-15 23:05:15 +0300

    - add list forbes

--- code.js
+++ code.js
@@ -1,34 +1,49 @@
 let generalList = ["вася", "петя", "маша", "даша", "женя"];
 let blackList = ["фома", "гия", "жора"];
+let listForbes = [ "хаби", "бузо","крид"];
 let userName;
 let decision = false;
 let message;
 
-function findNameInList() {
+function allListGuests() {
     userName = document.getElementById("name_user").value;
     for (let i = 0; i < generalList.length; i++) {
-
         if (userName === generalList[i]) {
-            decision = true;
-            // console.log(decision);
-        } // let message = ", УХОДИ";
-        findNameInBlackList();
-    }
+            decision = 1;
+        }else if (userName !== generalList[i]){
+            for (let j = 0; j < blackList.length; j++){
+                if (userName ===blackList[j]){
+                    decision = 2;
+                }
+            }
 
-    if (decision === true) {
-        message = ", ПРОХОДИ"
-    }
-    document.getElementById("showMessage").innerHTML = userName + message;
-}
+        }else{
+            for (let l = 0; l < listForbes; l++){
+                if (userName === listForbes[l]){
+                    decision = 3;
+                }
+            }
 
-
-function findNameInBlackList() {
-    for(let i = 0; i< blackList.length; i++){
-        if (userName === blackList[i]){
-            decision = true;
-            message = ", простите, мест нет"
+        }
         }
 
-    }
 
+    // for(let i = 0; i< blackList.length; i++){
+    //     if (userName === blackList[i]){
+    //         decision = true;
+    //     }
+    // }
+
+
+    //
+    // if (decision === true) {
+    //     message = ", ПРОХОДИ"
+    // }
+    // document.getElementById("showMessage").innerHTML = userName + message;
+
+    // switch (decision) {
+    //     case : 1
+    //
+    // }
 }
+
