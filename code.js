edd9b28f73e8329469e8db4fe20dea1f4698500c
KateKrav <laylovalova@gmail.com>
2020-10-15 15:17:06 +0300

    - function cycle "for"

--- code.js
+++ code.js
@@ -5,12 +5,14 @@ let findName = "";
 function findNameInList(){
     userName = document.getElementById("name_user").value ;
   for (let i = 0; i < generalList.length; i++){
+      findName = "";
+      findName = findName + generalList[i];
       // console.log(generalList[i]);
-      if (userName === generalList[i]){
+      if (userName === findName){
           console.log(userName + " проходи!");
       }else {
           console.log("Проваливай!")
       }
-
   }
+
 }
