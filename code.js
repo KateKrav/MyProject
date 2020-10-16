6f4997bcd06f708d0c775e53a1c7584f73b252bb
KateKrav <laylovalova@gmail.com>
2020-10-16 14:13:15 +0300

    - Return value

--- code.js
+++ code.js
@@ -1,49 +1,62 @@
 let generalList = ["вася", "петя", "маша", "даша", "женя"];
 let blackList = ["фома", "гия", "жора"];
-let listForbes = [ "хаби", "бузо","крид"];
+let listForbes = ["хаби", "бузо", "крид"];
 let userName;
 let decision = false;
 let message;
 
 function allListGuests() {
     userName = document.getElementById("name_user").value;
-    for (let i = 0; i < generalList.length; i++) {
-        if (userName === generalList[i]) {
-            decision = 1;
-        }else if (userName !== generalList[i]){
-            for (let j = 0; j < blackList.length; j++){
-                if (userName ===blackList[j]){
-                    decision = 2;
-                }
-            }
-
-        }else{
-            for (let l = 0; l < listForbes; l++){
-                if (userName === listForbes[l]){
-                    decision = 3;
-                }
-            }
-
-        }
-        }
-
-
-    // for(let i = 0; i< blackList.length; i++){
-    //     if (userName === blackList[i]){
-    //         decision = true;
-    //     }
-    // }
-
-
-    //
-    // if (decision === true) {
-    //     message = ", ПРОХОДИ"
-    // }
-    // document.getElementById("showMessage").innerHTML = userName + message;
-
-    // switch (decision) {
-    //     case : 1
-    //
-    // }
 }
 
+
+function returnTrue() {
+    return true;
+}
+
+function checkIsTrue_() {
+    if (returnTrue()) {
+        alert("checked function returns true")
+    }
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+result = sum( 5,3);
+console.log(result);
+
+let a = 1;
+let b = 6;
+
+
+function simile() {
+    if (a < b)
+    return true;
+
+}
+function checkIsTrue() {
+    if (simile()){
+        console.log("да")
+    }else {
+        console.log ("No")
+    }
+}
+
+
+let userNumber;
+  userNumber = document.getElementById("getNumber").value;
+function simileNumber() {
+    if (userName > b){
+        return true;
+    }
+}
+
+function checkSimile() {
+    if (simileNumber()){
+        console.log(`${userNumber}> заданого числа 6`)
+    }else {
+        console.log(`${userNumber}< заданого числа или равно 6`)
+    }
+}
