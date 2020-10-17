c913b36364fe0d6b0d3f9e00e66a4f51096d8713
KateKrav <laylovalova@gmail.com>
2020-10-17 21:25:48 +0300

    - function show list; - function search name.

--- faculty.js
+++ faculty.js
@@ -1,3 +1,48 @@
-let griffindor = [ "Альбус"];
-let slytherin = ["Сириус"];
-let ravenclaw = ["Седрик"];
+let griffindor = [ "Альбус", "Гарри", "Гермиона", "Рон"];
+let slytherin = ["Суверус", "Малфой", "Волдемор"];
+let ravenclaw = ["Седрик","Полумна","Миртл"];
+let listGriffindor = false;
+let listSlihterin = false;
+let listRavenclaw = false;
+let showlistGriffindor = "";
+let showlistSlihterin = "";
+let showlistRavenclaw= "";
+let searchName;
+
+
+
+
+function listGriffindorStudent() {
+    if (listGriffindor === false){
+        for ( let i = 0; i < griffindor.length; i++){
+            showlistGriffindor = showlistGriffindor + griffindor[i] + "<br>";
+           document.getElementById("list1").innerHTML = showlistGriffindor;
+        }
+        listGriffindor = true;
+    }
+
+}
+function listSlytherinStudent() {
+    if (listSlihterin === false){
+        for (let i = 0; i< slytherin.length; i++){
+           showlistSlihterin = showlistSlihterin + slytherin[i] + "<br>" ;
+           document.getElementById("list2").innerHTML = showlistSlihterin;
+        }
+        listSlihterin = true;
+    }
+    
+}
+function listRavenclawStudent() {
+    if (listRavenclaw === false){
+        for (let i = 0; i < ravenclaw.length; i++){
+            showlistRavenclaw = showlistRavenclaw + ravenclaw[i]+ "<br>";
+            document.getElementById("list3").innerHTML = showlistRavenclaw;
+        }
+        listRavenclaw = true;
+    }
+
+}
+function searchNameStudent() {
+
+
+}
