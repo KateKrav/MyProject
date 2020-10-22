e358b3d300f9a4189c14a402329d32740ca24217
KateKrav <laylovalova@gmail.com>
2020-10-22 23:46:05 +0300

    Revert "space"
    This reverts commit 5573e4c8fd2b370b8eec4c16ee88b8c265e4ec58.
    
    # Conflicts:
    #	faculty.js

--- faculty.js
+++ faculty.js
@@ -1,93 +1,119 @@
-let griffindor = ["Альбус", "Гарри", "Гермиона", "Рон", "Хагрид"];
+let griffindor = [ "Альбус", "Гарри", "Гермиона", "Рон", "Хагрид"];
 let slytherin = ["Суверус", "Малфой", "Волдемор"];
-let ravenclaw = ["Седрик", "Полумна", "Миртл"];
-let griffindor_decision = 0;
-let slitherin_decision = 0;
-let ravenclaw_decision = 0;
-let check1;
-let check2;
-let check3;
-let check4;
-let check5;
-let check6;
+let ravenclaw = ["Седрик","Полумна","Миртл"];
+let listGriffindor = false;
+let listSlihterin = false;
+let listRavenclaw = false;
+let showlistGriffindor = "";
+let showlistSlihterin = "";
+let showlistRavenclaw= "";
+let searchName;
+let hogwartsFaculty = [ griffindor, slytherin, ravenclaw];
 
-let featureGriffindor;
-let featureSlytherin;
-let featureRavenclaw;
-let features;
-// console.log(featureGriffindor);
+
+
+
+function listGriffindorStudent() {
+    if (listGriffindor === false){
+        for ( let i = 0; i < griffindor.length; i++){
+            showlistGriffindor = showlistGriffindor + griffindor[i] + "<br>";
+           document.getElementById("list1").innerHTML = showlistGriffindor;
+        }
+        listGriffindor = true;
+    }
+
+}
+function listSlytherinStudent() {
+    if (listSlihterin === false){
+        for (let i = 0; i< slytherin.length; i++){
+           showlistSlihterin = showlistSlihterin + slytherin[i] + "<br>" ;
+           document.getElementById("list2").innerHTML = showlistSlihterin;
+        }
+        listSlihterin = true;
+    }
+
+}
+function listRavenclawStudent() {
+    if (listRavenclaw === false){
+        for (let i = 0; i < ravenclaw.length; i++){
+            showlistRavenclaw = showlistRavenclaw + ravenclaw[i]+ "<br>";
+            document.getElementById("list3").innerHTML = showlistRavenclaw;
+        }
+        listRavenclaw = true;
+    }
+
+}
+
+let result = false;
+
+function getName() {
+    searchName = document.getElementById("inputSearch").value;
+}
+
+function notFoundName() {
+    if (searchName){
+        document.getElementById("hidden_message").hidden = false;
+        document.getElementById("GETmessage").innerHTML = "Имя не найдено";
+    }
+}
+
+function scanFaculty() {
+    getName();
+    for (let i = 0; i < hogwartsFaculty.length; i++) {
+        for (let j = 0; j < hogwartsFaculty[i].length; j++) {
+            if (hogwartsFaculty[i][j] === searchName) {
+                getMessage();
+                // console.log(searchName + ' учится здесь');
+                result = true;
+
+            }notFoundName();
+
+        }
+    }
+
+}
+
+function getMessage() {
+    document.getElementById("hidden_massege").hidden = false;
+    document.getElementById("GETmassege").innerHTML = searchName + ' учится здесь';
+}
+
 let newUserName;
 let check;
-
-function get_values() {
-    check1 = document.getElementById("1poi");
-    check2 = document.getElementById("2poi");
-    check3 = document.getElementById("3poi");
-    check4 = document.getElementById("4poi");
-    check5 = document.getElementById("5poi");
-    check6 = document.getElementById("6poi");
-    featureGriffindor = [check1.checked, check2.checked];
-    featureSlytherin = [check3.checked, check4.checked];
-    featureRavenclaw = [check5.checked, check6.checked];
-    features = [featureGriffindor, featureSlytherin, featureRavenclaw];
+check1 = document.getElementById("1poi");
+check2 = document.getElementById("2poi");
+check3 = document.getElementById("3poi");
+check4 = document.getElementById("4poi");
+check5 = document.getElementById("5poi");
+check6 = document.getElementById("6poi");
 
 
-}
-
+let featureGriffindor =[check1.checked , check4.checked];
+let featureSlytherin =[check2 , check6];
+let featureRavenclaw =[check3 , check5];
 
 function findYourFaculty() {
-    for (let i = 0; i < features.length; i++) {
-for (let j = 0; j < features[i].length; j++){
-    if (featureGriffindor[j]) {
-        griffindor_decision = griffindor_decision + 1;
-    } else if (featureSlytherin[j]) {
-        slitherin_decision = slitherin_decision + 1;
-    } else if (featureRavenclaw[j]) {
-        ravenclaw_decision = ravenclaw_decision + 1;
-    }
-}
-
-    }
+   for (let i = 0; i < featureGriffindor; i++ ){
+       if (featureGriffindor === true){
+           console.log("грифиндор")
+       }
+   }
 }
 
 function addNewStudent() {
-    get_values();
     findYourFaculty();
-    introduction();
-    // check = document.getElementById("1poi");
-    //  if (check.checked === true){
-    //      console.log("11111")
-    //  }
+   // check = document.getElementById("1poi");
+   //  if (check.checked === true){
+   //      console.log("11111")
+   //  }
 }
+
+
+
+
 
 
 //гриф.=1poi,4poi; слиз= 7poi, когт= 3poi,5poi;
 function test() {
-    console.dir(document.body);
-}
-
-function introduction() {
-if (griffindor_decision > slitherin_decision && griffindor_decision > ravenclaw_decision){
-    console.log("вы приняты в гриффиндор!")
-}
-
-else if (slitherin_decision > griffindor_decision && slitherin_decision > ravenclaw_decision){
-    console.log("вы приняты в слизерин!")
-}
-
-else if (ravenclaw_decision > griffindor_decision && ravenclaw_decision > slitherin_decision){
-    console.log("вы приняты в когтевран!")
-}
-
-else if (griffindor_decision === slitherin_decision){
-    console.log("вы можете выбрать между гриффиндомром и слизерином")
-}
-else if (griffindor_decision === ravenclaw_decision){
-    console.log("вы можете выбрать между гриффиндомром и когтевраном")
-}
-
-else if (slitherin_decision === ravenclaw_decision){
-    console.log("вы можете выбрать между слизерином и когтевраном")
-}
-
+    console.dir( document.body);
 }
