a84192b5bdc9819c12ca89705b894eccba1c048e
allekso <ajjekso@gmail.com>
2020-10-22 23:45:27 +0300

    fixed merge request

--- faculty.js
+++ faculty.js
@@ -10,6 +10,48 @@ let check3;
 let check4;
 let check5;
 let check6;
+let listGriffindor = false;
+let listSlihterin = false;
+let listRavenclaw = false;
+let showlistGriffindor = "";
+let showlistSlihterin = "";
+let showlistRavenclaw= "";
+let searchName;
+let hogwartsFaculty = [ griffindor, slytherin, ravenclaw];
+
+
+
+
+function listGriffindorStudent() {
+    if (listGriffindor === false){
+        for ( let i = 0; i < griffindor.length; i++){
+            showlistGriffindor = showlistGriffindor + griffindor[i] + "<br>";
+           document.getElementById("list1").innerHTML = showlistGriffindor;
+        }
+        listGriffindor = true;
+    }
+
+}
+function listSlytherinStudent() {
+    if (listSlihterin === false){
+        for (let i = 0; i< slytherin.length; i++){
+           showlistSlihterin = showlistSlihterin + slytherin[i] + "<br>" ;
+           document.getElementById("list2").innerHTML = showlistSlihterin;
+        }
+        listSlihterin = true;
+    }
+
+}
+function listRavenclawStudent() {
+    if (listRavenclaw === false){
+        for (let i = 0; i < ravenclaw.length; i++){
+            showlistRavenclaw = showlistRavenclaw + ravenclaw[i]+ "<br>";
+            document.getElementById("list3").innerHTML = showlistRavenclaw;
+        }
+        listRavenclaw = true;
+    }
+
+}
 
 let featureGriffindor;
 let featureSlytherin;
