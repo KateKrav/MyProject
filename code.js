791ecac6aa1b68bfcf420f86e830c955632fca44
KateKrav <laylovalova@gmail.com>
2020-10-29 21:22:53 +0200

    -add function add items

--- code.js
+++ code.js
@@ -1,37 +1,14 @@
-let new_arr = [ 1, "text", 20, 15];
+let listChildren = ["Вася","Маша", "Саша", "Паша", "Софи", "Маша", "Гоша", "Марго", "Макс", "Аня",];
 
-function funPush() {
-    new_arr.push("newText");
-    console.log(new_arr);
+let teamOne = [];
+let teamTwo = [];
+
+function addInTeamOne() {
+    teamOne = listChildren.splice(0, 1);
+    document.getElementById("team_1").innerHTML = teamOne + ' в команде 1';
+}
+function addInTeamTwo() {
+    teamTwo = listChildren.splice(0, 1);
+    document.getElementById("team_2").innerHTML = teamTwo + ' в команде 2';
 }
 
-function funPop() {
-    new_arr.pop();
-    console.log(new_arr);
-}
-
-function funShift() {
-    new_arr.shift();
-    console.log(new_arr);
-}
-
-function funUnshift() {
-    new_arr.unshift("Kakulina");
-    console.log(new_arr);
-}
-
-let box = ["apple", "book", "pencil","glasses", "old newspaper"];
-function deleteElement() {
-    box.splice(3,1);
-    console.log(box);
-}
-
-function replaceElement(){
-    box.splice(1, 3, "Рыба", "Носок", "12", "Лодка", "Свитер");
-    console.log(box);
-}
-
-function addElements() {
-
-
-}
